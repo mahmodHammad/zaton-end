62c0be77819abf782d739341d910894dd68e3dca
mahmoudHammad <41790090+mahmodHammad@users.noreply.github.com>
2020-03-22 21:06:07 +0200

    adding middle ware for auth

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,27 @@ const express = require('express')
 const app =express()
 
 
+const FBauth =(req,res ,next)=>{
+    let idToken ;
+    if (req.headers.auth){
+        idToken = req.headers.auth
+    }
+    else{
+        console.error("NO Token found")
+        return res.status(403).json({error:"unAuthorized"})
+    }
+
+    admin.auth().verifyIdToken(idToken).then(decodedToken => {
+        console.log(decodedToken)
+        req.user = decodedToken;
+       return next()
+    }).catch(err=>{
+        console.error(err , "ERROR WHILE BARIFYING TOKEN")
+        res.status(400).json({body:"body must not be empty"})
+    })
+}
+
+
 app.get('/videos' , (req, res) => {
   admin
     .firestore()
@@ -22,7 +43,7 @@ app.get('/videos' , (req, res) => {
 });
 
 
-app.post('/videos',(req, res) => {
+app.post('/videos' ,FBauth,(req, res) => {
   const newVideo = {
     title: "Signal Analysis",
     value: [
